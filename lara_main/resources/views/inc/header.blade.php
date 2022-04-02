214f3faab5bb5c355506d24e9244821cb03ad4d1
xombol <cobl-m@mail.ru>
2022-04-03 00:39:52 +0300

    new

--- lara_main/resources/views/inc/header.blade.php
+++ lara_main/resources/views/inc/header.blade.php
@@ -1,3 +1,17 @@
+@if (Route::has('login'))
+    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+        @auth
+            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Profile</a>
+        @else
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+            @endif
+        @endauth
+    </div>
+@endif
+@livewireScripts
 <header class="py-5">
     <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
         <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
--- lara_main/routes/web.php
+++ lara_main/routes/web.php
@@ -1,9 +1,33 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/laravel', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
 
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+
 Route::get('/about', function () {
     return view('about');
 })->name('about');
